11597219558d5a65f5bd5a6a279e86a4754e13a8
AbelSu <abelsu1993@outlook.com>
2016-03-20 13:44:21 +0800

    Danmu_3
    Danmu_3

--- SuDanmu/ViewController.m
+++ SuDanmu/ViewController.m
@@ -34,9 +34,75 @@
         [newArr addObject:img];
     }
     _imgView.animationImages = newArr;
-    _
+    _imgView.animationDuration = 20;
+    [_imgView startAnimating];
+    
+    CGRect rect = CGRectMake(0, 2, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds)-4);
+    DanmakuConfiguration *configuration = [[DanmakuConfiguration alloc]init];
+    configuration.duration = 6.5;
+    configuration.paintHeight = 21;
+    configuration.fontSize = 17;
+    configuration.largeFontSize = 19;
+    configuration.maxLRShowCount = 30;
+    configuration.maxShowCount = 45;
+    _danmakuView = [[DanmakuView alloc]initWithFrame:rect Configuration:configuration];
+    _danmakuView.delegate = self;
+    [self.view insertSubview:_danmakuView aboveSubview:_imgView];
+    
+    // 引用字幕文件
+    NSString *danmakufile = [[NSBundle mainBundle]pathForResource:@"danmakufile" ofType:nil];
+    NSArray *danmakus = [NSArray arrayWithContentsOfFile:danmakufile];
+    [_danmakuView prepareDanmakus:danmakus];
     
 }
+
+- (void)onTimeCount
+{
+    _slider.value += 0.1/120;
+    if (_slider.value > 120.0) {
+        _slider.value = 0;
+    }
+    [self onTimeChange:nil];
+}
+
+- (IBAction)onTimeChange:(id)sender {
+    _curTime.text = [NSString stringWithFormat:@"%0.fs",_slider.value*120.0];
+    
+}
+
+- (IBAction)onStartClick:(id)sender {
+    if (_danmakuView.isPrepared) {
+        if (!_timer) {
+            _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(onTimeCount) userInfo:nil repeats:YES];
+        }
+        [_danmakuView start];
+    }
+}
+
+- (IBAction)onPauseClick:(id)sender {
+    if (_timer) {
+        [_timer invalidate];
+        _timer = nil;
+    }
+    [_danmakuView pause];
+}
+
+- (IBAction)onSendClick:(id)sender {
+    int time = ([self danmakuViewGetPlayTime:nil]+1)*1000;
+    int type = rand()%3;
+    NSString *pString = [NSString stringWithFormat:@"%d,%d,1,00EBFF,125",time,type];
+    NSString *mString = @"😊😊online.com😊😊";
+    DanmakuSource *danmakeSource = [DanmakuSource createWithP:pString M:mString];
+    [_danmakuView sendDanmakuSource:danmakeSource];
+    
+}
+
+#pragma mark -
+- (float)danmakuViewGetPlayTime:(DanmakuView *)danmakuView{
+    return _slider.value*120.0;
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
